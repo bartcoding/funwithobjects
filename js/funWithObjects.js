2188519e059c605160071607cf7c570fbc74c7e9
BartIntec <bart.hunerbein@intecbrussel.be>
2021-03-09 10:41:51 +0100

    added the person information to the dom via some functions

--- js/funWithObjects.js
+++ js/funWithObjects.js
@@ -8,12 +8,50 @@ let person4 = new Person('billy', 20, 'playing cownboy and indian');
 
 let person5 = new Person('bronco', 10, 'riding the minimotorcycle');
 
-renderPeople();
+renderPeople(person1,person2,person3,person4,person5);
 
 function renderPeople(person1,person2,person3,person4,person5){
 
     //add the persons to the div with id people
+    addPersonToPeopleDiv(person1);
+    addPersonToPeopleDiv(person2);
+    addPersonToPeopleDiv(person3);
+    addPersonToPeopleDiv(person4);
+    addPersonToPeopleDiv(person5);
+}
 
+function addPersonToPeopleDiv(person){
+    let persondiv = document.createElement('div');
+    let colour = getRandomColor();
+    persondiv.style.borderColor = colour;
+    persondiv.style.borderStyle = 'solid';
+    
+    
+    //added the name of the person to a new p element
+    let nameParagraph = document.createElement('p');
+    let nameText = document.createTextNode(person.name);
+    nameParagraph.appendChild(nameText);
+    //added the age of the person to a new p element
+    let ageParagraph = document.createElement('p');
+    let ageText = document.createTextNode(person.age);
+    ageParagraph.appendChild(ageText);
+    //added the hobby of the person to a new p element
+    let hobbyParagraph = document.createElement('p');
+    let hobbyText = document.createTextNode(person.hobby);
+    hobbyParagraph.appendChild(hobbyText);
+
+    //add a button to call the introduction function
+    let buttonelement = document.createElement('button');
+    buttonelement.addEventListener('click',person.introduction);
+    buttonelement.textContent = 'click to hear an introduction';
+
+    persondiv.appendChild(nameParagraph);
+    persondiv.appendChild(ageParagraph);
+    persondiv.appendChild(hobbyParagraph);
+    persondiv.appendChild(buttonelement);
+
+    let peoplediv = document.querySelector('#people');
+    peoplediv.appendChild(persondiv);
 }
 
 /*
@@ -45,9 +83,9 @@ function Person(name, age, hobby){
     this.name = name;
     this.age = age;
     this.hobby = hobby;
-    this.indroduction = function (){
+    this.introduction = function (){
         alert("Hi! nice to meet you, my name is " + this.name + ", I am " + this.age + "years old and my hobby is: " + this.hobby);
-    }
+    };
     //add method that shouts introduces the person like: hello my name is name, my age is age and my main hobby is hobby
 
 
@@ -69,7 +107,14 @@ function personCreateFunction(name, age, hobby){
 
 
 
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 
 
